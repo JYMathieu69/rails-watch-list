9a91bb9b05fc6ea7cfd7da6db7f5ee5cf7b72fca
Mathieu Jolly <mathieujolly69@gmail.com>
2021-05-21 17:03:42 +0200

    ok

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks
+  has_many :bookmarks, dependent: :destroy
+
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  validates :overview, presence: true
 end
